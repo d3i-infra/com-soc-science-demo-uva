b9a97e2c92c66f59d2a0d6438d25908d08e8343d
Theo Araujo <theoaraujo@users.noreply.github.com>
2023-03-16 12:12:45 +0100

    Update splash_screen.tsx

--- src/framework/visualisation/react/ui/pages/splash_screen.tsx
+++ src/framework/visualisation/react/ui/pages/splash_screen.tsx
@@ -55,23 +55,20 @@ export const SplashScreen = (props: Props): JSX.Element => {
     <>
       <div className='text-bodylarge font-body text-grey1'>
         <div className='mb-4 text-bodylarge font-body text-grey1'>
-          You are about to start the process of donating your data to research institute ASCoR at Amsterdam University. The data that we ask you to donate will be used for academic research to gain insight into how social media platforms work.
+          You are about to start the process of donating your data for the lecture on tracking from the Computational Social Science bachelor at the UvA. The data that we ask you to donate will be used in class to gain insight into how social media platforms work.
         </div>
         <div className='mb-4 text-bodylarge font-body text-grey1'>
-          We will walk you through this process step by step. During this process no data is stored or sent to ASCoR. You can delete rows from the data before donating. Data will only be donated and stored when you click the button “Yes, donate” on the page that shows your data.
+          We will walk you through this process step by step. During this process no data is stored or sent to the lecturers. You can delete rows from the data before donating. Data will only be donated and stored when you click the button “Yes, donate” on the page that shows your data.
         </div>
         <div className='mb-6 text-bodylarge font-body text-grey1'>
           By clicking the button “<span className='font-bodybold'>Yes, donate</span>”:
         </div>
         <div className='flex flex-col gap-3 mb-6'>
           <Bullet>
-            <div>you fully and voluntarily agree to donate your data for this research.</div>
+            <div>you fully and voluntarily agree to donate your data for this lecture.</div>
           </Bullet>
           <Bullet>
-            <div>you are aware that when your data is used for academic publications, or made publicly available in some other form, this will be anonymous.</div>
-          </Bullet>
-          <Bullet>
-            <div>you are aware that you have the right to withdraw your permission within 7 days by contacting Panel Inzicht.</div>
+            <div>you are aware that your data will be anonymously donated and only used in the context of the lecture.</div>
           </Bullet>
         </div>
         <div className='mb-10'>
@@ -85,27 +82,25 @@ export const SplashScreen = (props: Props): JSX.Element => {
     <>
       <div className='text-bodylarge font-body text-grey1'>
         <div className='mb-4'>
-          U kunt zo uw gegevens gaan doneren voor een onderzoek van onderzoeksinstituut ASCoR aan de Universiteit van Amsterdam. De gegevens die we u vragen te doneren worden gebruikt voor wetenschappelijke onderzoek om inzicht te krijgen in de werkwijze van sociale media.
+        You are about to start the process of donating your data for the lecture on tracking from the Computational Social Science bachelor at the UvA. The data that we ask you to donate will be used in class to gain insight into how social media platforms work.
         </div>
         <div className='mb-4'>
-          We leggen u stap voor stap wat er van u verwacht wordt. Tijdens deze stappen worden geen gegevens opgeslagen of naar ASCoR verstuurd. U kunt zelf rijen uit uw data verwijderen die u niet wilt doneren. Pas als u de vraag krijgt of u de gegevens wilt doneren en u op de knop “Ja, doneer” klikt, worden de gegevens gedoneerd en opgeslagen.
+        We will walk you through this process step by step. During this process no data is stored or sent to the lecturers. You can delete rows from the data before donating. Data will only be donated and stored when you click the button “Yes, donate” on the page that shows your data.
         </div>
         <div className='mb-4'>
-          Door op de knop “<span className='font-bodybold'>Ja, doneer</span>” te klikken:
+          By clicking on the button “<span className='font-bodybold'>Ja, doneer</span>”:
         </div>
         <div className='flex flex-col gap-3 mb-6'>
           <Bullet>
-            <div>Geeft u volledig en vrijwillig toestemming om uw data te doneren voor dit onderzoek.'</div>
+            <div>you fully and voluntarily agree to donate your data for this lecture.</div>
           </Bullet>
           <Bullet>
-            <div>Geeft u aan te weten dat als uw gegevens worden gebruikt in wetenschappelijke publicaties, of deze op een andere manier openbaar worden gemaakt, dit dan anoniem gebeurt.</div>
+            <div>you are aware that your data will be anonymously donated and only used in the context of the lecture.</div>
           </Bullet>
-          <Bullet>
-            <div>Geeft u aan te weten dat u het recht hebt om uw toestemming binnen 7 dagen in te trekken door contact met Panel Inzicht op te nemen.</div>
-          </Bullet>
+
         </div>
         <div className='mb-10'>
-          Deze website houdt ook uw activiteiten bij – bijvoorbeeld op welke pagina’s van deze website u klikt – als deel van dit onderzoek. U kunt meer informatie op onze privacy pagina vinden.
+        This website keeps track of your activity - for example on which pages of this website you click - as part of this research. More information can be found on our privacy page.
         </div>
       </div>
     </>
